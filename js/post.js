81038311fd398a7add3828d3f855ba97ea3588ae
mismelpoulout <69745379+mismelpoulout@users.noreply.github.com>
2021-10-06 08:53:21 -0300

    Add files via upload

---
+++ js/post.js
@@ -0,0 +1,12 @@
+
+  var VUUKLE_CONFIG = {
+    apiKey: '5eb0a5d3-a560-41ae-a727-325299812fc6',
+    articleId: 'Generate Unique id for your article',
+  };
+  // ⛔️ DON'T EDIT BELOW THIS LINE
+  (function() {
+    var d = document,
+      s = d.createElement('script');
+    s.src = 'https://cdn.vuukle.com/platform.js';
+    (d.head || d.body).appendChild(s);
+  })();
